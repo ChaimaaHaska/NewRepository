8da79d2a789d418e47b7f9ccc520d45fe1c9828e
chaimaa haska <haska.chaimaa@gmail.com>
2019-11-20 01:09:04 +0100

    first-commit

---
+++ src/org/sid/Produit.java
@@ -0,0 +1,40 @@
+package org.sid;
+
+public class Produit {
+	
+	private int id ; 
+	private String name ;
+	private int PrixEuro;
+	
+	
+	public Produit(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getPrixEuro() {
+		return PrixEuro;
+	}
+	public void setPrixEuro(int prixEuro) {
+		PrixEuro = prixEuro;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	} 
+	
+	public int ToDh() {
+		return this.PrixEuro*10;
+	}
+	
+	
+
+}
---
+++ src/org/sid/ProduitTest.java
@@ -0,0 +1,18 @@
+package org.sid;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class ProduitTest {
+
+	Produit p = new Produit(1, "produit 1");
+	@Test
+	void testToDh() {
+		p.setPrixEuro(12);
+		assertEquals(120, p.ToDh());
+		
+	}
+
+}
